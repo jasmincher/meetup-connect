165823daa2e671b68d0fb68c1c8f7cdf11903f54
Jas <c.jasminher@gmail.com>
2019-06-19 10:46:52 -0400

    fix: Changed header img location

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Header.css';
-
+import tech from '../images/women-in-tech.jpg';
 
 
 const Header = () => (
 
-    <div className="header">
+    <div className="header" style={{backgroundImage: "url(" + tech + ")" }}>
         <div className="header-overlay">
             <Link to="/" style={{textDecoration: 'none'}}>
                 <h1 className="header-title">Meetup Connect</h1>
