e96ca26421b3015643b5b05c0b98262e9686299b
Jas <c.jasminher@gmail.com>
2019-06-19 17:07:50 -0400

    fix: Added more styling to label and inputs for the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,29 +10,29 @@ const Form = () => (
 
 
             <div className="form-group">
-                <input type="text" className="form-control" id="input1" placeholder="Enter First Name" />
+                <input type="text" className="form-control" id="input1" placeholder="First Name" />
                 <label for="input1">First Name</label>
             </div>
 
 
             <div className="form-group">
-                <input type="text" className="form-control" id="input2" placeholder="Enter Last Name" />
+                <input type="text" className="form-control" id="input2" placeholder="Last Name" />
                 <label for="input2">Last Name</label>
             </div>
 
 
             <div className="form-group">
-                <input type="email" className="form-control" id="input3" placeholder="Enter Email" />
+                <input type="email" className="form-control" id="input3" placeholder="Email" />
                 <label for="input3">Email</label>
             </div>
 
             <div className="form-group">
-                <input type="text" className="form-control" id="input4" placeholder="Enter City" />
+                <input type="text" className="form-control" id="input4" placeholder="City" />
                 <label for="input4">City</label>
             </div>
 
             <div className="form-group">
-                <input type="text" className="form-control" id="input4" placeholder="Enter Interest" />
+                <input type="text" className="form-control" id="input4" placeholder="Interest" />
                 <label for="input4">Interest</label>
             </div>
 
