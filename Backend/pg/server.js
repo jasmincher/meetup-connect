54c6064cc5461be6945c91b0ab76557c2775f8fc
Geidy <geidydm@gmail.com>
2019-06-20 12:32:59 -0400

    Updated backend file

--- Backend/pg/server.js
+++ Backend/pg/server.js
@@ -35,67 +35,86 @@ db.connect()
 .then( res => console.log('connected to database'))
 .catch( err => console.log(err));
 
-//   query = "SELECT * From WomenTech WHERE interest = 'React'";
-//   //console.log(err);
-//   client.query(query)
-//    .then(res => { 
-//     for(var i = 0; i < resizeBy.rowCount; i++){
-//       console.log(res.rows[i].FirstName + " " + res.rows[i].LastName + " " + res.rows[i].interest)}
+ 
+//for(var i = 0; i < resizeBy.rowCount; i++){
+//console.log(res.rows[i].FirstName + " " + res.rows[i].LastName + " " + res.rows[i].interest)}
 
-//   } 
-// )
-//   .catch(e => console.error(e.stack));
-// });
+
+// frontend: axios.get(;='localhost:4000/getUserInfo--/)
 //Get information from a form an update database
-app.get('/getNewInfo'), function(req, res){
+//const UserInfo =[];
+app.get('/UserInfo'), function(req, response) {
+  console.log('getting a request');
+  //database calls queries
+  db.query("Insert \"FirstName \"LastName \"Email \"location \"Interest \"INTO \"WomenTech",
+  function (err, res, fields){
+    if(err) throw err;
+    console.log(res.rows);
+    var dataInfo =[];
+    for(i=0;i<rows.length;i++){
+      dataInfo.push(rows[i].FirstName + " " + rows[i].LastName + " "
+       + rows[i].Email + " " + rows[i].Location + " " + rows[i].Interest);
+    }
+    //respond to the request
+    response.send(res.rows);
+    //res.end(JSON.stringify(datInfo));
+  })
   
-}
+  };
+
+  //Post method to get info aout of database to interact with React
+  app.post('UserInfo', function(req, response){
+    db.query("Insert \"FirstName \"LastName \"Email \"Location \"Interest \"WomenTech",
+    function (err, res, fields){
+      if(err) throw err;
+      console.log(res.rows)
+      var dataInfo = [];
+      for(i=0;i<rows.length;i++){
+        dataInfo.push(rows[i].FirstName + " " + rows[i].LastName + " "
+        + rows[i].Email + rows[i].Location + " " + rows[i].Interest);
+      }
+      //response to the request
+      response.send(res.rows);
+      //res.end(JSON.stringify(dataInfo));
+    })  
+    
+
+  });
 
 
-
-//Get method to access database and return results for users with same interes(in Json)
+//Get method to access database and return results for users with same interest(in Json)
 app.get('/search/:interest', function(req, response){
   console.log('getting a request');
   db.query("Select * from \"WomenTech\" WHERE \"Interest\" LIKE '%React%'",
     function(err, res, fields){
       if(err) throw err;
       console.log(res.rows);
-      // var data = [];
-      // for(i=0;i<rows.length;i++){
-      //   data.push(rows[i].FirstName + " " + rows[i].LastName + " " + rows[i].interest);
-      // }
+       var data = [];
+       for(i=0;i<rows.length;i++){
+         data.push(rows[i].FirstName + " " + rows[i].LastName + " " + rows[i].Interest);
+       }
       response.send(res.rows);
       // res.end(JSON.stringify(data));
     });
   });
 
   //Post method to access database and get results for equal interest
-  app.post('/search', function(req, res){ //POST method to access DB and return results in JSON
-    db.query('SELECT * FROM WomenTech WHERE Interest LIKE "%' + req.params.interest + '%"',
+  app.post('/search', function(req, response){ //POST method to access DB and return results in JSON
+    db.query("Select * from \" WomenTech\" WHERE\" Interest\" LIKE ' '%' + req.params.Interest + '%'",
     function(err, rows, fields){
       if(err) throw err;
       var data = [];
       for(i=0;i<rows.length;i++){
         data.push(rows[i].FirstName + " " + rows[i].LastName + " " + rows[i].Email);
       }
-      res.end(JSON.stringify(data));
-      console.log(req.params.input);
+      response.send(JSON.stringify(data));
+      console.log(req.params.Interest);
     });
   });
  
 
 
-
-// frontend: axios.get(;='localhost:4000/getWome--/)
-const getWomenInterest = {};
-app.get('/getWomenInterest/:interest', function(request, response) {
-  // database calls (queries)
-
-//respond to the request
-  response.send('hey')
-});
-
-app.listen(4000, function() {
+  app.listen(4000, function() {
   console.log('listening on port 4000');
 })
 
