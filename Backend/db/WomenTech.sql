91d28b65840a49685123d823bab50bfecfc63210
Geidy <geidydm@gmail.com>
2019-06-20 17:02:11 -0400

    database id updated

--- Backend/db/WomenTech.sql
+++ Backend/db/WomenTech.sql
@@ -1,29 +1,3 @@
-﻿INSERT INTO "WomenTech"(
-            id, "FirstName", "LastName", "Email", "Image", "Location", "Interest")
-    VALUES (21, 'Geidy', 'Molina', 'gei@yahoo.com', 'url', 'Bronx', 'Cyber Security');
-INSERT INTO "WomenTech"(
-             id, "FirstName", "LastName", "Email", "Image", "Location", "Interest") 
-    VALUES (12, 'Jasmine', 'Choo', 'jcho@gmail.com', 'url', 'Manhattan', 'HTML & CSS');   
-INSERT INTO "WomenTech"(
-             id, "FirstName", "LastName", "Email", "Image", "Location", "Interest") 
-    VALUES (8, 'Elena', 'Jason', 'eleja@hotmail.com', 'url', 'Brooklyn', 'React'); 
-INSERT INTO "WomenTech"(
-             id, "FirstName", "LastName", "Email", "Image", "Location", "Interest") 
-    VALUES (27, 'Anabel', 'Perez', 'anap@yahoo.com', 'url', 'Manhattan', 'HTML & CSS'); 
-INSERT INTO "WomenTech"(
-             id, "FirstName", "LastName", "Email", "Image", "Location", "Interest") 
-    VALUES (31, 'Simone', 'Well', 'wells@hotmail.com', 'url', 'Staten Island', 'JavaScript'); 
-INSERT INTO "WomenTech"(
-             id, "FirstName", "LastName", "Email", "Image", "Location", "Interest") 
-    VALUES (42, 'Claudia', 'Gonzales', 'claugz@gmail.com', 'url', 'Queens', 'Phyton'); 
-INSERT INTO "WomenTech"(
-             id, "FirstName", "LastName", "Email", "Image", "Location", "Interest") 
-    VALUES (18, 'Susset', 'Monrroy', 'monrroy@yahoo.com', 'url', 'Manhattan', 'C++'); 
-INSERT INTO "WomenTech"(
-             id, "FirstName", "LastName", "Email", "Image", "Location", "Interest") 
-    VALUES (15, 'Maylen', 'Choo', 'jcho@yahoo.com', 'url', 'Bronx', 'React'); 
-INSERT INTO "WomenTech"(
-             id, "FirstName", "LastName", "Email", "Image", "Location", "Interest") 
-    VALUES (20, 'Elena', 'Troya', 'eltro@hotmail.com', 'url', 'Queens', 'C++');  
-       
-                                    
+﻿SELECT "FirstName", "LastName", "Email", "Image", "Location", "Interest", 
+       id
+  FROM "WomenTech";
